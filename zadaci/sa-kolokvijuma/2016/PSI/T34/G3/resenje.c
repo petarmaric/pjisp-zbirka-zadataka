387c95a31878c406b8666b8c33b9f89853423f43
Petar Mari? <petar.maric@gmail.com>
2017-01-31 23:57:08 +0100

    zadaci: Reformatirani svi moji predlozi re?enja tako da koriste 4 razmaka za uvla?enje programskog koda, umesto 1 tab

--- zadaci/sa-kolokvijuma/2016/PSI/T34/G3/resenje.c
+++ zadaci/sa-kolokvijuma/2016/PSI/T34/G3/resenje.c
@@ -7,17 +7,17 @@
 #define MAX_DIJAGNOZA 20+1
 
 struct pacijent_st {
-	char ime[MAX_IME];
-	char prezime[MAX_IME];
-	unsigned masa;
-	double visina;
+    char ime[MAX_IME];
+    char prezime[MAX_IME];
+    unsigned masa;
+    double visina;
 };
 
 struct analiza_st {
-	char ime[MAX_IME];
-	char prezime[MAX_IME];
-	double bmi;
-	char dijagnoza[MAX_DIJAGNOZA];
+    char ime[MAX_IME];
+    char prezime[MAX_IME];
+    double bmi;
+    char dijagnoza[MAX_DIJAGNOZA];
 };
 
 
@@ -29,87 +29,87 @@ void snimi_analize(FILE *out, struct analiza_st analize[], int n);
 
 
 int main(int arg_num, char **args) {
-	if (arg_num != 3) {
-		printf("USAGE: %s IN_FILENAME OUT_FILENAME\n", args[0]);
-		exit(1);
-	}
+    if (arg_num != 3) {
+        printf("USAGE: %s IN_FILENAME OUT_FILENAME\n", args[0]);
+        exit(1);
+    }
 
-	char *in_filename = args[1];
-	char *out_filename = args[2];
+    char *in_filename = args[1];
+    char *out_filename = args[2];
 
-	FILE *in  = safe_fopen(in_filename,  "r", 1);
-	FILE *out = safe_fopen(out_filename, "w", 2);
+    FILE *in  = safe_fopen(in_filename,  "r", 1);
+    FILE *out = safe_fopen(out_filename, "w", 2);
 
-	struct pacijent_st pacijenti[MAX_NIZ];
-	struct analiza_st analize[MAX_NIZ];
-	int n;
+    struct pacijent_st pacijenti[MAX_NIZ];
+    struct analiza_st analize[MAX_NIZ];
+    int n;
 
-	ucitaj_pacijente(in, pacijenti, &n);
-	transform(pacijenti, analize, n);
-	snimi_analize(out, analize, n);
+    ucitaj_pacijente(in, pacijenti, &n);
+    transform(pacijenti, analize, n);
+    snimi_analize(out, analize, n);
 
-	fclose(in);
-	fclose(out);
+    fclose(in);
+    fclose(out);
 
-	return 0;
+    return 0;
 }
 
 FILE *safe_fopen(char filename[], char mode[], int error_code) {
-	FILE *fp = fopen(filename, mode);
-	if(fp == NULL) {
-		printf("Can't open '%s'!", filename);
-		exit(error_code);
-	}
-	return fp;
+    FILE *fp = fopen(filename, mode);
+    if(fp == NULL) {
+        printf("Can't open '%s'!", filename);
+        exit(error_code);
+    }
+    return fp;
 }
 
 void ucitaj_pacijente(FILE *in, struct pacijent_st pacijenti[], int *n) {
-	*n = 0;
-	while(fscanf(
-		in, "%s %s %u %lf",
-		 pacijenti[*n].ime,
-		 pacijenti[*n].prezime,
-		&pacijenti[*n].masa,
-		&pacijenti[*n].visina
-	) != EOF) {
-		(*n)++;
-	}
+    *n = 0;
+    while(fscanf(
+        in, "%s %s %u %lf",
+         pacijenti[*n].ime,
+         pacijenti[*n].prezime,
+        &pacijenti[*n].masa,
+        &pacijenti[*n].visina
+    ) != EOF) {
+        (*n)++;
+    }
 }
 
 double odredi_bmi(struct pacijent_st pacijent) {
-	return pacijent.masa / (pacijent.visina * pacijent.visina);
+    return pacijent.masa / (pacijent.visina * pacijent.visina);
 }
 
 void transform(struct pacijent_st pacijenti[], struct analiza_st analize[], int n) {
-	int i;
-	for(i=0; i<n; i++) {
-		strcpy(analize[i].ime, pacijenti[i].ime);
-		strcpy(analize[i].prezime, pacijenti[i].prezime);
+    int i;
+    for(i=0; i<n; i++) {
+        strcpy(analize[i].ime, pacijenti[i].ime);
+        strcpy(analize[i].prezime, pacijenti[i].prezime);
 
-		double bmi = odredi_bmi(pacijenti[i]);
-		analize[i].bmi = bmi;
-		
-		if (bmi < 18.5) {
-			strcpy(analize[i].dijagnoza, "Pothranjenost");
-		} else if (bmi < 25.0) {
-			strcpy(analize[i].dijagnoza, "Idealna tezina");
-		} else if (bmi < 30.0) {
-			strcpy(analize[i].dijagnoza, "Prekomerna tezina");
-		} else {
-			strcpy(analize[i].dijagnoza, "Gojaznost");
-		}
-	}
+        double bmi = odredi_bmi(pacijenti[i]);
+        analize[i].bmi = bmi;
+        
+        if (bmi < 18.5) {
+            strcpy(analize[i].dijagnoza, "Pothranjenost");
+        } else if (bmi < 25.0) {
+            strcpy(analize[i].dijagnoza, "Idealna tezina");
+        } else if (bmi < 30.0) {
+            strcpy(analize[i].dijagnoza, "Prekomerna tezina");
+        } else {
+            strcpy(analize[i].dijagnoza, "Gojaznost");
+        }
+    }
 }
 
 void snimi_analize(FILE *out, struct analiza_st analize[], int n) {
-	int i;
-	for(i=0; i<n; i++) {
-		fprintf(
-			out, "%s %s %.2f %s\n",
-			analize[i].ime,
-			analize[i].prezime,
-			analize[i].bmi,
-			analize[i].dijagnoza
-		);
-	}
+    int i;
+    for(i=0; i<n; i++) {
+        fprintf(
+            out, "%s %s %.2f %s\n",
+            analize[i].ime,
+            analize[i].prezime,
+            analize[i].bmi,
+            analize[i].dijagnoza
+        );
+    }
 }
--- zadaci/sa-kolokvijuma/2016/PSI/T5/G3/resenje.c
+++ zadaci/sa-kolokvijuma/2016/PSI/T5/G3/resenje.c
@@ -7,141 +7,141 @@
 #define MAX_PLATFORMA 8+1
 
 typedef struct igra_st {
-	char igra[MAX_IGRA];
-	char zanr[MAX_ZANR];
-	char platforma[MAX_PLATFORMA];
-	double cena;
-	struct igra_st *next;
+    char igra[MAX_IGRA];
+    char zanr[MAX_ZANR];
+    char platforma[MAX_PLATFORMA];
+    double cena;
+    struct igra_st *next;
 } IGRA;
 
 void init_list(IGRA **head) {
-	*head = NULL;
+    *head = NULL;
 }
 
 void add_to_list(IGRA *new, IGRA **head) {
-	if(*head == NULL) { // list is empty
-		*head = new;
-		return;
-	} 
-	
-	add_to_list(new, &((*head)->next));
+    if(*head == NULL) { // list is empty
+        *head = new;
+        return;
+    } 
+    
+    add_to_list(new, &((*head)->next));
 }
 
 IGRA *create_new_item(char igra[], char zanr[], char platforma[], double cena) {
-	IGRA *new = (IGRA *)malloc(sizeof(IGRA));
-	if (new == NULL) {
-		printf("Not enough RAM!\n");
-		exit(21);
-	}
+    IGRA *new = (IGRA *)malloc(sizeof(IGRA));
+    if (new == NULL) {
+        printf("Not enough RAM!\n");
+        exit(21);
+    }
 
-	strcpy(new->igra, igra);
-	strcpy(new->zanr, zanr);
-	strcpy(new->platforma, platforma);
-	new->cena = cena;
+    strcpy(new->igra, igra);
+    strcpy(new->zanr, zanr);
+    strcpy(new->platforma, platforma);
+    new->cena = cena;
 
-	new->next = NULL;
+    new->next = NULL;
 
-	return new;
+    return new;
 }
 
 void read_list_from(FILE *in, IGRA **head) {
-	char tmp_igra[MAX_IGRA];
-	char tmp_zanr[MAX_ZANR];
-	char tmp_platforma[MAX_PLATFORMA];
-	double tmp_cena;
-	
-	while(fscanf(in, "%s %s %s %lf", tmp_igra, tmp_zanr, tmp_platforma, &tmp_cena) != EOF) {
-		IGRA *new = create_new_item(tmp_igra, tmp_zanr, tmp_platforma, tmp_cena);
-		add_to_list(new, head);
-	}
+    char tmp_igra[MAX_IGRA];
+    char tmp_zanr[MAX_ZANR];
+    char tmp_platforma[MAX_PLATFORMA];
+    double tmp_cena;
+    
+    while(fscanf(in, "%s %s %s %lf", tmp_igra, tmp_zanr, tmp_platforma, &tmp_cena) != EOF) {
+        IGRA *new = create_new_item(tmp_igra, tmp_zanr, tmp_platforma, tmp_cena);
+        add_to_list(new, head);
+    }
 }
 
 void save_item_to(FILE *out, IGRA *x) {
-	fprintf(
-		out, "%-10s %-8s %-8s %5.2f\n", 
-		x->igra, x->zanr, x->platforma, x->cena
-	);
+    fprintf(
+        out, "%-10s %-8s %-8s %5.2f\n", 
+        x->igra, x->zanr, x->platforma, x->cena
+    );
 }
 
 void save_list_to(FILE *out, IGRA *head) {
-	if(head != NULL) {
-		save_item_to(out, head);
-		save_list_to(out, head->next);
-	}
+    if(head != NULL) {
+        save_item_to(out, head);
+        save_list_to(out, head->next);
+    }
 }
 
 void destroy_list(IGRA **head) {
-	if(*head != NULL) {
-		destroy_list(&((*head)->next));
-		free(*head);
-		*head = NULL;
-	}
+    if(*head != NULL) {
+        destroy_list(&((*head)->next));
+        free(*head);
+        *head = NULL;
+    }
 }
 
 FILE *safe_fopen(char *filename, char *mode, int error_code) {
-	FILE *fp = fopen(filename, mode);
-	if (fp == NULL) {
-		printf("Can't open '%s'!\n", filename);
-		exit(error_code);
-	}
-	return fp;
+    FILE *fp = fopen(filename, mode);
+    if (fp == NULL) {
+        printf("Can't open '%s'!\n", filename);
+        exit(error_code);
+    }
+    return fp;
 }
 
 IGRA *get_najpovoljnija_igra(IGRA *head, char zanr[], char platforma[]) {
-	if (head == NULL) { // list is empty
-		return NULL;
-	}
+    if (head == NULL) { // list is empty
+        return NULL;
+    }
 
-	IGRA *best = NULL;
-	while(head != NULL) {
-		if (strcmp(head->zanr, zanr) == 0 && strcmp(head->platforma, platforma) == 0) { // Gledamo samo igre koje su OK
-			if (best == NULL) { // Pre ovoga sigurno nije bilo igre koja je OK
-				best = head;
-			} else if (head->cena < best->cena) { // Nadjena povoljnija igra, koja je OK
-				best = head;
-			}
-		}
-		
-		head = head->next;
-	}
+    IGRA *best = NULL;
+    while(head != NULL) {
+        if (strcmp(head->zanr, zanr) == 0 && strcmp(head->platforma, platforma) == 0) { // Gledamo samo igre koje su OK
+            if (best == NULL) { // Pre ovoga sigurno nije bilo igre koja je OK
+                best = head;
+            } else if (head->cena < best->cena) { // Nadjena povoljnija igra, koja je OK
+                best = head;
+            }
+        }
+        
+        head = head->next;
+    }
 
-	return best;
+    return best;
 }
 
 int main(int arg_num, char *args[]) {
-	if (arg_num != 5) {
-		printf("USAGE: %s PLATFORMA ZANR IN_FILENAME OUT_FILENAME\n", args[0]);
-		exit(11);
-	}
+    if (arg_num != 5) {
+        printf("USAGE: %s PLATFORMA ZANR IN_FILENAME OUT_FILENAME\n", args[0]);
+        exit(11);
+    }
 
-	char *platforma = args[1];
-	char *zanr = args[2];
-	char *in_filename = args[3];
-	char *out_filename = args[4];
+    char *platforma = args[1];
+    char *zanr = args[2];
+    char *in_filename = args[3];
+    char *out_filename = args[4];
 
-	FILE *in  = safe_fopen(in_filename,  "r", 1);
-	FILE *out = safe_fopen(out_filename, "w", 2);
+    FILE *in  = safe_fopen(in_filename,  "r", 1);
+    FILE *out = safe_fopen(out_filename, "w", 2);
 
-	IGRA *head;
-	init_list(&head);
+    IGRA *head;
+    init_list(&head);
 
-	read_list_from(in, &head);
-	save_list_to(out, head);
+    read_list_from(in, &head);
+    save_list_to(out, head);
 
-	IGRA *best = get_najpovoljnija_igra(head, zanr, platforma);
-	if (best == NULL) {
-		fprintf(out, "\nZa %s platformu ne postoje '%s' igre!\n", platforma, zanr);
-	} else {
-		fprintf(
-			out, "\nNajpovoljnija '%s' igra za %s platformu je:\n%s %.2f\n",
-			zanr, platforma, best->igra, best->cena
-		);
-	}
+    IGRA *best = get_najpovoljnija_igra(head, zanr, platforma);
+    if (best == NULL) {
+        fprintf(out, "\nZa %s platformu ne postoje '%s' igre!\n", platforma, zanr);
+    } else {
+        fprintf(
+            out, "\nNajpovoljnija '%s' igra za %s platformu je:\n%s %.2f\n",
+            zanr, platforma, best->igra, best->cena
+        );
+    }
 
-	destroy_list(&head);
+    destroy_list(&head);
 
-	fclose(in);
-	fclose(out);
+    fclose(in);
+    fclose(out);
 
-	return 0;
+    return 0;
 }
