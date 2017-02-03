672367cb0d34b0829049330baf96fdc563ecc355
Petar Mari? <petar.maric@gmail.com>
2017-02-03 01:03:21 +0100

    LaTeX: Smanjena veli?ina fonta za listinge programskog koda, da bi listinzi ?irine 80 karaktera lepo stali na stranice knjige

--- conf.py
+++ conf.py
@@ -131,6 +131,8 @@ latex_elements = {
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
+
+    'preamble': r'\fvset{fontsize=\scriptsize}',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
