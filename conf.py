e6c8e018d557814485122fa951d826ca653ab9ba
Petar Mari? <petar.maric@gmail.com>
2017-02-04 23:56:53 +0100

    LaTeX: Promenjena pode?avanja 'fncychap' LaTeX paketa - aka fensi-?mensi izgled naziva poglavlja

--- conf.py
+++ conf.py
@@ -120,6 +120,7 @@ latex_elements = {
     'pointsize': '10pt',
     'geometry': r'\usepackage[papersize={165mm,235mm}, margin=20mm]{geometry}',
 
+    'fncychap': r'\usepackage[Lenny]{fncychap}',
     'fontpkg': r"""
         \usepackage{lmodern}
         \IfFileExists{zi4.sty}{\usepackage{zi4}}{\usepackage{inconsolata}} % 'Inconsolata' is my monospace font of choice
