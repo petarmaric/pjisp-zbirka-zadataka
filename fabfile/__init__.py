cd3a8e8d7c32bc247c3444cfa608631ed310ee54
Petar Marić <petar.maric@gmail.com>
2017-03-13 19:58:06 +0100

    Created a 'help' Fabric task

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,2 +1,17 @@
+"""\
+Pregled korisnih komandi:
+
+* 'fab build'                         kompajlira celokupnu knjigu (u svim podrzanim formatima)
+* 'fab novi_zadatak.sa_kolokvijuma'   dodaje novi zadatak sa kolokvijuma
+"""
+
+from fabric.api import task
+
 import build
 import novi_zadatak
+
+
+@task(default=True)
+def help():
+    from fabric.main import show_commands
+    show_commands(__doc__, 'normal')
