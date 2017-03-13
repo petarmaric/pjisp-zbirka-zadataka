2ee280acbfaf20c631ac9e40925e384a3fbe7466
Petar Mari? <petar.maric@gmail.com>
2017-03-13 19:53:58 +0100

    Reorganized Fabric tasks into independent Python modules

--- fabfile/novi_zadatak.py
+++ fabfile/novi_zadatak.py
@@ -1,9 +1,7 @@
 from datetime import date
-import fnmatch
 import os
-import shutil
 
-from fabric.api import abort, env, local, prompt, task
+from fabric.api import abort, prompt, task
 from jinja2 import Environment, FileSystemLoader
 
 
@@ -14,15 +12,6 @@ VALID_GRUPE = ["G%d" % x for x in xrange(1, MAX_GRUPA+1)]
 
 ZADACI_BASE_DIR = 'zadaci'
 ZADACI_SA_KOLOKVIJUMA_DIR = os.path.join(ZADACI_BASE_DIR, 'sa-kolokvijuma')
-
-SPHINX_SOURCE_DIR = '.'
-SPHINX_BUILD_DIR = '_build'
-
-SPHINX_BUILDERS = [
-    'html',
-    'singlehtml',
-    'text',
-]
 
 
 def validate_choice(choices, coerce=None):
@@ -43,7 +32,7 @@ def prompt_choice(question, choices, coerce=None):
     )
 
 @task
-def novi_zadatak_sa_kolokvijuma():
+def sa_kolokvijuma():
     today = date.today()
     skolska_godina = today.year
     if today.month < 10: # Smatraj da tek od oktobra pocinje nova skolska godina
@@ -92,71 +81,5 @@ Ukoliko je to potrebno (i tacno znate sta radite) mozete napraviti i dodatne izm
 
 OBAVEZNO temeljno testirajte 'resenje.c'!
 
-Celokupnu knjigu potom mozete ponovo kompajlirati pozivom komande 'fab'.
+Celokupnu knjigu potom mozete ponovo kompajlirati pozivom komande 'fab build'.
 """ % zadatak_dir
-
-
-@task
-def clean():
-    if os.path.exists(SPHINX_BUILD_DIR):
-        print "Directory '%s' already exists and will be deleted before continuing" % SPHINX_BUILD_DIR
-        shutil.rmtree(SPHINX_BUILD_DIR)
-        print
-        print
-
-    os.mkdir(SPHINX_BUILD_DIR)
-
-
-@task
-def sphinx_build(builder_name):
-    print "Building %s..." % builder_name
-    local("sphinx-build -M %s %s %s" % (builder_name, SPHINX_SOURCE_DIR, SPHINX_BUILD_DIR))
-    print
-    print
-    print
-
-
-@task
-def build_books():
-    sphinx_build('latex')
-
-    print 'Building books...'
-    env.is_miktex = is_miktex()
-    
-    os.chdir(os.path.join(SPHINX_BUILD_DIR, 'latex'))
-    for filename in fnmatch.filter(os.listdir('.'), '*.tex'):
-        pdflatex(filename)
-        makeindex(filename)
-        pdflatex(filename)
-        pdflatex(filename)
-    
-    print "Build finished; the PDF files are in %s." % os.path.join(SPHINX_BUILD_DIR, 'latex')
-
-def is_miktex():
-    return 'MiKTeX' in local('pdflatex -version', capture=True)
-
-def pdflatex(filename):
-    local("pdflatex %(be_quiet)s -interaction=nonstopmode -halt-on-error %(filename)s" % {
-        'filename': filename,
-        'be_quiet': '-quiet' if env.is_miktex else '' # TexLive (used by Ubuntu) doesn't support the '-quiet' CLI flag
-    })
-
-def makeindex(filename):
-    local("makeindex -q %s.idx" % os.path.splitext(filename)[0])
-
-
-@task
-def lint():
-    sphinx_build('lint')
-
-
-@task(default=True)
-def rebuild_all():
-    clean()
-
-    lint()
-
-    for builder_name in SPHINX_BUILDERS:
-        sphinx_build(builder_name)
-
-    build_books()
