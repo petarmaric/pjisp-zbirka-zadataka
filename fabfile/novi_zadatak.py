84309ceb4951fc9c07576a7cc01ee6b91d3e3a70
Petar Marić <petar.maric@gmail.com>
2018-09-11 08:39:56 +0200

    Dodato zadavanje autora zadatka u 'novi_zadatak.sa_kolokvijuma' Fabric task

--- fabfile/novi_zadatak.py
+++ fabfile/novi_zadatak.py
@@ -1,3 +1,4 @@
+import codecs
 from datetime import date
 import locale
 import os
@@ -13,6 +14,7 @@ MAX_GRUPA = 9
 MAX_GRUPA_STARI_STUDENTI = 4
 VALID_GRUPE = ["G%d" % x for x in xrange(1, MAX_GRUPA+1)] + \
               ["S%d" % x for x in xrange(1, MAX_GRUPA_STARI_STUDENTI+1)]
+AUTHORS_LIST = [line.strip() for line in codecs.open('AUTHORS', 'r', 'utf-8')]
 
 ZADACI_BASE_DIR = 'zadaci'
 ZADACI_SA_KOLOKVIJUMA_DIR = os.path.join(ZADACI_BASE_DIR, 'sa-kolokvijuma')
@@ -31,9 +33,20 @@ def validate_choice(choices, coerce=None):
 
 def prompt_choice(question, choices, coerce=None):
     return prompt(
-        text="%s (%s)? " % (question, ', '.join(choices)),
+        text="%s (%s)? " % (question, ', '.join(choices).encode('utf-8')),
         validate=validate_choice(choices, coerce)
     )
+
+def validate_author(author):
+    matches = [x for x in AUTHORS_LIST if x.lower().startswith(author.lower())]
+
+    num_matches = len(matches)
+    if num_matches == 0:
+        raise ValueError("No authors found that start with '%s'" % author)
+    if num_matches > 1:
+        raise ValueError("Multiple matching authors found (%s)" % ', '.join(matches))
+
+    return matches[0]
 
 @task
 def sa_kolokvijuma():
@@ -49,6 +62,7 @@ def sa_kolokvijuma():
     kolokvijum = prompt_choice('Koji kolokvijum', VALID_KOLOKVIJUMI, str.upper)
     grupa = prompt_choice('Koja grupa', VALID_GRUPE, str.upper)
     naziv = prompt('Unesite kratak i opisan naziv zadatka:')
+    autor = prompt_choice('Ko je autor zadatka', AUTHORS_LIST, validate_author)
 
     print '\nPravim zadatak:'
 
@@ -63,6 +77,7 @@ def sa_kolokvijuma():
     def create_file_from_template(filename):
         env.get_template(filename).stream({
             'naziv': naziv.decode(sys.stdin.encoding or locale.getpreferredencoding(True)), # based on http://stackoverflow.com/a/477496
+            'autor': autor,
             'zadatak_dir': zadatak_dir.replace('\\', '/'),
         }).dump(os.path.join(zadatak_dir, filename), encoding='utf-8')
 
