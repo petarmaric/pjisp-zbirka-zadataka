799d3dc8c92a52378901c2a80751ec2c62ce0253
Petar Mari? <petar.maric@gmail.com>
2018-11-02 15:18:07 +0100

    zadaci: Dodat tekst zadatka sa kolokvijuma 2018 PSI T12 G1, uz moj predlog resenja

---
+++ zadaci/sa-kolokvijuma/2018/PSI/T12/G1/index.rst
@@ -0,0 +1,16 @@
+.. index::
+   Algoritmi ; Numerika
+
+
+Zadatak "Na kraju, može biti samo jedan!"
+=========================================
+
+.. sectionauthor:: Petar Marić <petarmaric@uns.ac.rs>
+
+.. include:: zadatak.rst
+
+
+Primer rešenja
+--------------
+
+.. literalinclude:: resenje.c
---
+++ zadaci/sa-kolokvijuma/2018/PSI/T12/G1/resenje.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    do {
+        printf("Unesite broj clanova reda: ");
+        scanf("%d", &n);
+    } while(n<1);
+
+    int i, j;
+    double brojilac, imenilac, clan;
+    double sum = 0;
+    for(i=0; i<n; i++) {
+        brojilac = 1;
+
+        imenilac = (i+1) * (i+2);
+
+        clan = brojilac/imenilac;
+        printf("clan[i=%d] = %lf\n", i, clan);
+
+        sum += clan;
+    }
+
+    printf("\nsum = %lf\n", sum);
+
+    return 0;
+}
